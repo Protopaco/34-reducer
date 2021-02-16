5238593acb067aa85196a5efa408b1906f0e6a4c
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-16 13:29:16 -0800

    first refactor

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
+import ColorPicker from '../../containers/ColorPicker'
 
 function App() {
-  const { current, undo, redo, record } = useRecord('#FF0000');
-
-  return (
-    <>
-      <button onClick={undo}>undo</button>
-      <button onClick={redo}>redo</button>
-      <input
-        type="color"
-        value={current}
-        onChange={({ target }) => record(target.value)} />
-      <div
-        style={{ backgroundColor: current, width: '10rem', height: '10rem' }}>
-
-      </div>
-    </>
-  );
+  return <ColorPicker />
 }
 
 export default App;
---
+++ src/containers/ColorPicker.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import useRecord from '../hooks/useRecord'
+
+export const ColorPicker = () => {
+    const { current, undo, redo, record } = useRecord('#FF0000');
+
+    return (
+        <>
+            <button onClick={undo}>undo</button>
+            <button onClick={redo}>redo</button>
+            <input
+                type="color"
+                value={current}
+                onChange={({ target }) => record(target.value)} />
+            <div
+                style={{ backgroundColor: current, width: '10rem', height: '10rem' }}>
+
+            </div>
+        </>
+    );
+}
+
+export default ColorPicker;
